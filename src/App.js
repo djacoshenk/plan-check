dc38585ea0e731870374dc9fa41d15dd8bd8a5ff
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-06-24 10:36:16 -0700

    updated logo path

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://weplancheck.com/static/media/logo.5d5d9eef.svg" className="App-logo" alt="logo" />
         <p>
-          Coming Soon
+          A web application for architects and engineers to upload project documents, track review status, and receive revision comments.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
