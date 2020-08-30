9d67a11f45dfcb68347cdd24cdc28e54be40a665
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-08-29 22:45:58 -0700

    added tailwind css

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='bg-blue-800 text-white h-screen text-4xl flex flex-row items-center justify-center'>
+      Coming Soon...
+    </div>
+  );
+}
+
+export default App;
