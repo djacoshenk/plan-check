e5a2d67fd4e2ef0815350c27b28eca080b35b96a
Danny Jacoshenk <hello@dannyjaco.me>
2021-05-30 20:03:18 -0700

    Add UserSignUpPage and UserDashboardPage to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { HomePage } from 'pages/HomePage/HomePage';
+import { UserDashboardPage } from 'pages/UserDashboardPage/UserDashboardPage';
 import { UserSignInPage } from 'pages/UserSignInPage/UserSignInPage';
+import { UserSignUpPage } from 'pages/UserSignUpPage/UserSignUpPage';
 
 export function App() {
   return (
@@ -14,6 +16,12 @@ export function App() {
           </Route>
           <Route path='/signin'>
             <UserSignInPage />
+          </Route>
+          <Route path='/signup'>
+            <UserSignUpPage />
+          </Route>
+          <Route path='/user'>
+            <UserDashboardPage />
           </Route>
         </Switch>
       </BrowserRouter>
