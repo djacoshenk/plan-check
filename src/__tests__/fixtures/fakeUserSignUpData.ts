b6f82fb87f92d7e4e5527f2dbbd07b253fa487ef
Danny Jacoshenk <hello@dannyjaco.me>
2021-06-01 12:37:04 -0700

    Add fixtures data for user sign up test

---
+++ src/__tests__/fixtures/fakeUserSignUpData.ts
@@ -0,0 +1,7 @@
+export const fakeUserSignUpData = {
+  first_name: 'Danny',
+  last_name: 'Jacoshenk',
+  email: 'daniel.jacoshenk@gmail.com',
+  password: 'password',
+  confirm_password: 'password',
+};
