79def601603a154d5de0f6e08d8ad8836ec05559
Danny Jacoshenk <hello@dannyjaco.me>
2021-06-08 22:44:18 -0700

    Delete tailwind components and tests

--- src/__tests__/fixtures/fakeUserSignUpData.ts
+++
@@ -1,7 +0,0 @@
-export const fakeUserSignUpData = {
-  first_name: 'Danny',
-  last_name: 'Jacoshenk',
-  email: 'daniel.jacoshenk@gmail.com',
-  password: 'password',
-  confirm_password: 'password',
-};
--- src/pages/UserSignInPage/UserSignInPage.tsx
+++
@@ -1,5 +0,0 @@
-import { UserSignInPageSignInForm } from 'pages/UserSignInPage/UserSignInPageSignInForm';
-
-export function UserSignInPage() {
-  return <UserSignInPageSignInForm />;
-}
