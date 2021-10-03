fa460c5f733c741dcd3349b15948e37eb049dde8
Danny Jacoshenk <hello@dannyjaco.me>
2021-10-02 17:34:12 -0700

    Add color prop to Spinner component

--- src/pages/UserSignIn/UserSignInForm.tsx
+++ src/pages/UserSignIn/UserSignInForm.tsx
@@ -200,7 +200,7 @@ export function UserSignInForm() {
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                {isSigningIn ? <Spinner /> : "Sign in"}
+                {isSigningIn ? <Spinner color="#fff" /> : "Sign in"}
               </button>
             </div>
           </form>
