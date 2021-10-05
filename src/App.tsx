9e57cfb2ea47ad6ff3821ffa3957f6e1be23282d
Danny Jacoshenk <hello@dannyjaco.me>
2021-10-05 12:06:10 -0700

    Add AppProvider and Routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { Fragment } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AppProvider } from "context/AppProvider";
+import { Routes } from "routes/Routes";
 
-import { Home } from "pages/Home/Home";
-import { UserDashboard } from "pages/UserDashboard/UserDashboard";
-import { UserSignIn } from "pages/UserSignIn/UserSignIn";
-import { UserSignUp } from "pages/UserSignUp/UserSignUp";
-
-export function App() {
+export const App = () => {
   return (
-    <Fragment>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/signin">
-            <UserSignIn />
-          </Route>
-          <Route path="/signup">
-            <UserSignUp />
-          </Route>
-          <Route path="/user/:id">
-            <UserDashboard />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Fragment>
+    <>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+    </>
   );
-}
+};
